a212e79bd927aea428cee8f752f4968f74b6f654
mstasiak98 <mikistasiak98@gmail.com>
2023-09-17 18:04:44 +0200

    add storage service unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,42 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {ChecklistService} from "./shared/data-access/checklist.service";
+import {ChecklistItemService} from "./checklist/data-access/checklist-item.service";
+import {StorageService} from "./shared/data-access/storage.service";
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  // Mock services
+  const checklistServiceMock: Partial<ChecklistService> = {
+    load: jasmine.createSpy('load'),
+  };
+
+  const checklistItemServiceMock: Partial<ChecklistItemService> = {
+    load: jasmine.createSpy('load'),
+  };
+
+  const storageServiceMock: Partial<StorageService> = {};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [
+        { provide: ChecklistService, useValue: checklistServiceMock },
+        { provide: ChecklistItemService, useValue: checklistItemServiceMock },
+        { provide: StorageService, useValue: storageServiceMock },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
   });
 
   it('should create the app', () => {
@@ -17,5 +44,13 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should call load method for checklist service', () => {
+    expect(checklistServiceMock.load).toHaveBeenCalled();
+  })
+
+  it('should call load method for checklist item service', () => {
+    expect(checklistItemServiceMock.load).toHaveBeenCalled();
+  })
 
 });
--- src/app/shared/data-access/storage.service.spec.ts
+++ src/app/shared/data-access/storage.service.spec.ts
@@ -1,16 +1,74 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Storage } from "@ionic/storage-angular";
 import { StorageService } from './storage.service';
+import {Checklist} from "../models/checklist";
+import {of} from "rxjs";
 
 describe('StorageService', () => {
   let service: StorageService;
+  let storage: Storage;
+
+  const testLoadData: any = {};
+
+  const setMock = jasmine.createSpy('set');
+  const getMock = jasmine.createSpy('get').and.resolveTo(testLoadData);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Storage,
+          useValue: {
+            create: jasmine.createSpy('create').and.resolveTo({
+              set: setMock,
+              get: getMock,
+            } as any),
+          },
+        },
+        StorageService
+      ],
+      teardown: {
+        destroyAfterEach: true
+      }
+    });
+
     service = TestBed.inject(StorageService);
+    storage = TestBed.inject(Storage);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('loadChecklists$', () => {
+    it('should return result of get method of storage api', (done) => {
+      service.loadChecklists$.subscribe((result) => {
+        expect(getMock).toHaveBeenCalledWith('checklists' as any);
+        expect(result).toEqual(testLoadData);
+        done();
+      })
+    })
+  })
+
+  describe('saveChecklists()', () => {
+
+    beforeEach(() => {
+      service = TestBed.inject(StorageService);
+      storage = TestBed.inject(Storage);
+    });
+
+    it('should pass data to set method of storage api', () => {
+      const testData = {} as any;
+      service.loadChecklists$.subscribe(() => {
+        service.saveChecklists(testData);
+        expect(setMock).toHaveBeenCalledWith('checklists' as any, testData);
+      })
+    })
+
+    it('should NOT pass data if checklists have not been loaded yet', () => {
+      const testData = {} as any;
+      service.saveChecklists(testData);
+      expect(setMock).not.toHaveBeenCalled();
+    });
+  })
 });
